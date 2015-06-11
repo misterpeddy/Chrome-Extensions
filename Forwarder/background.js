62e119dbac56e9a59bdc61d580f9fb58cedb0111
Pedram Pejman <pedrampejman2010@gmail.com>
2015-06-11 15:08:26 -0700

    Added HTTP status check before forwarding

--- Forwarder/background.js
+++ Forwarder/background.js
@@ -2,26 +2,48 @@ chrome.commands.onCommand.addListener(function() {
 	
 	chrome.tabs.query({active: true}, function(tabs) {
 		url = tabs[0].url;
-		newUrl = getNewUrl(url);
-		if (newUrl) {
-			chrome.tabs.update(tabs[0].id, {url: newUrl});
-		}
+		newUrl = getNewUrl(url, function(newUrl) {
+			if (newUrl) {
+				chrome.tabs.update(tabs[0].id, {url: newUrl});
+			}
+		});
 	});
 });
 
-function getNewUrl(url) {
+function getNewUrl(url, callback) {
 //Look for $[.*]/page/[.*
 page = url.substring(url.lastIndexOf("/page/")+6);
 if (!isNaN(parseInt(page))) {
-	return url.substring(0 , url.lastIndexOf("/page/")+6) + (parseInt(page) + 1);
+	callback(url.substring(0 , url.lastIndexOf("/page/")+6) + (parseInt(page) + 1));
+	return;
 }
-//Construct a pattern
+else {
+	//finish with a slash
+	if (url.charAt(url.length -1 ) != "/") {
+		url = url + "/";
+	}
+	url = url + "page/2";
 
-//finish with a slash
-if (url.charAt(url.length -1 ) != "/") {
-	url = url + "/";
+	validate(url, function(request) {
+		chrome.extension.getBackgroundPage().console.log(request);
+		if (request.status == 200) {
+			callback(url);
+			return;
+		} 
+		else {
+			chrome.extension.getBackgroundPage().console.log("Cannot go forward");
+		}
+	});
 }
-url = url + "page/2";
-return url;
+}
 
+function validate(url, callback) {
+	var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, true );
+    xmlHttp.onreadystatechange = function() {
+    	if (xmlHttp.readyState == 4) {
+    		callback(xmlHttp);
+    	}
+    }
+    xmlHttp.send();
 }
